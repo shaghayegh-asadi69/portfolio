5ee7d3a77d3de09b2dac50350264f7b5d7d9e3b9
Shaghayegh Asadi <shaghayegh.asadi69@yahoo.com>
2023-09-13 22:34:26 +0100

    new

--- portfolio-reactt/src/components/portfolio/Menu.jsx
+++ portfolio-reactt/src/components/portfolio/Menu.jsx
@@ -1,19 +1,18 @@
 const Menu = [
-    {
-        id: 1,
-        title: 'project Management Illuatration',
-        category: 'Design'
-    },
-    {
-        id: 2,
-        title: 'Spotifind',
-        category: 'Front end'
-    },
-    {
-        id: 3,
-        title: '',
-        category: 'Design'
-    }
-    
-    
+  {
+    id: 1,
+    title: "planet donation",
+    category: "Design",
+  },
+  {
+    id: 2,
+    title: "Spotifind",
+    category: "Front end",
+  },
+  {
+    id: 3,
+    title: "Beasts",
+    category: "Design",
+  },
 ];
+export default Menu;
--- portfolio-reactt/src/components/portfolio/Portfolio.jsx
+++ portfolio-reactt/src/components/portfolio/Portfolio.jsx
@@ -8,15 +8,30 @@ const Portfolio = () => {
   return (
     <section className="work container section" id="work">
       <h2 className="section__title">Resent Works</h2>
-
       <div className="work__filters">
         <span className="work__item">Everything</span>
         <span className="work__item">Creative</span>
         <span className="work__item">Art</span>
         <span className="work__item">Design</span>
       </div>
-
-
+      <div className="work__container grid">
+        {items.map((elem) => {
+            const { id, title, category } = elem;
+            return (
+              <div className="work__card" key={id}>
+                <div className="work__thumbnail">
+                  <div className="work__mask"></div>
+                </div>
+              <span className="work__category">{category}</span>
+              <h3 className="work__title">{title}</h3>
+              <a href="#" className="work__button">
+                  <i className="icon-link work__button-icon"></i>
+              </a>
+              </div>
+      
+            )
+           })}
+    </div>
     </section>
   );
 };
